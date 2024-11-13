7b70e440fa931f3e2176490e7d8576223cc8d9bf
Samantha Walsh <swalsh2024@gmail.com>
2024-11-12 19:28:07 -0500

    hi

--- whackamole.py
+++ whackamole.py
@@ -2,6 +2,8 @@ import pygame
 
 
 def main():
+    moleX = 0
+    moleY = 0
     try:
         pygame.init()
         # You can draw the mole with this snippet:
@@ -14,7 +16,14 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN(event.pos)
             screen.fill("light green")
+            for x in range(1, 20):
+                pygame.draw.line(screen, "black", (32*x, 0), (32*x, 512))
+            for y in range(1, 16):
+                pygame.draw.line(screen, "black", (0, 32*y), (640, 32*y))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(moleX, moleY)))
+
             pygame.display.flip()
             clock.tick(60)
     finally:
